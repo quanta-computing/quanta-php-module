e477e1fda58c442d6ad1b5740d658b038a12f5e2
Matthieu Rosinski <korrigan@slash16.org>
2016-06-24 17:42:13 +0200

    fix potential issue in _safe_call_function when function is not callable

--- extension/src/call_function_helpers.c
+++ extension/src/call_function_helpers.c
@@ -8,9 +8,7 @@ size_t call_params_count, zval **call_params TSRMLS_DC) {
   zval is_callable;
   zval func;
 
-  INIT_ZVAL(func);
   MAKE_STD_ZVAL(params[0]);
-  ZVAL_NULL(&is_callable);
   ZVAL_STRING(&func, "is_callable", 1);
   if (object) {
     array_init(params[0]);
@@ -20,12 +18,17 @@ size_t call_params_count, zval **call_params TSRMLS_DC) {
     ZVAL_STRING(params[0], function_name, 1);
   }
   ret = call_user_function(CG(function_table), NULL, &func, &is_callable, 1, params TSRMLS_CC);
-  FREE_ZVAL(params[0]);
-  if (ret != SUCCESS || Z_TYPE(is_callable) != IS_BOOL || !Z_BVAL(is_callable))
-    return -1;
+  if (ret != SUCCESS || Z_TYPE(is_callable) != IS_BOOL || !Z_BVAL(is_callable)) {
+    ret = FAILURE;
+    goto end;
+  }
   ZVAL_STRING(&func, function_name, 1);
   ret = call_user_function(object ? NULL : CG(function_table), object ? &object : NULL,
     &func, ret_val, call_params_count, call_params TSRMLS_CC);
+
+end:
+  FREE_ZVAL(params[0]);
+  zval_dtor(&is_callable);
   zval_dtor(&func);
   return ret != SUCCESS || Z_TYPE_P(ret_val) != ret_type ? -1 : 0;
 }
