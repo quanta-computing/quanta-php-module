9b8c513e0e44101e6ba1f13ede0fee2df7326f12
Kraoz <main.kraoz@gmail.com>
2016-07-05 15:15:04 +0200

    added magento 2 reindex event

--- extension/src/benchmark.c
+++ extension/src/benchmark.c
@@ -8,33 +8,117 @@
 #include <sys/time.h>
 #include "computed_functions_filter.c"
 
-char *get_functions(void) {
-  char *content;
-  int fd;
-  struct stat sb;
+int count_lines(char *str) {
+  int count = 0;
+  while (*str) {
+    if (*str == '\n')
+      ++count;
+    ++str;
+  }
+  return count;
+}
 
-  fd = open("/Users/Kraoz/Downloads/suce.log", O_RDONLY);
+struct method {
+  char *function_name;
+  char *class_name;
+};
+
+struct method *get_methods(void) {
+  struct stat sb;
+  char *content;
+  struct method *lines;
+  struct method *ret;
+  int fd;
+
+  fd = open("/Users/Kraoz/Downloads/suce-1.log", O_RDONLY);
   fstat(fd, &sb);
   content = malloc(sb.st_size);
   read(fd, content, sb.st_size);
-  return content;
+  ret = lines = malloc((count_lines(content) + 1) * sizeof(*lines));
+  lines->function_name = content;
+  while (*content) {
+    if (*content == ' ') {
+      *content = '\0';
+      lines->class_name = content + 1;
+    } else if (*content == '\n') {
+      *content = '\0';
+      ++lines;
+      lines->function_name = content + 1;
+    }
+    ++content;
+  }
+  lines->function_name = NULL;
+  return ret;
+}
+
+#define INDEX_2_BYTE(index)  (index >> 3)
+#define INDEX_2_BIT(index)   (1 << (index & 0x7));
+#define QUANTA_MON_MAX_MONITORED_FUNCTIONS_HASH  256
+#define QUANTA_MON_MAX_MONITORED_FUNCTIONS  23
+#define QUANTA_MON_MONITORED_FUNCTION_FILTER_SIZE ((QUANTA_MON_MAX_MONITORED_FUNCTIONS_HASH + 7)/8)
+
+typedef unsigned long ulong;
+
+inline uint8_t hp_inline_hash(char * str) {
+  ulong h = 5381;
+  uint i = 0;
+  uint8_t res = 0;
+
+  while (*str)
+    h = (h + (h << 5)) ^ (ulong)*str++;
+
+  for (i = 0; i < sizeof(ulong); i++)
+    res += ((uint8_t *)&h)[i];
+  return res;
+}
+
+void hp_monitored_functions_filter_init(char **functions, uint8_t *monitored_function_filter) {
+  for(int i = 0; functions[i] != NULL; i++) {
+    if (!*functions[i]) continue;
+    char *str  = strstr(functions[i], "::");
+    uint8_t hash = hp_inline_hash(str ? str + 2 : functions[i]);
+    int idx = INDEX_2_BYTE(hash);
+    monitored_function_filter[idx] |= INDEX_2_BIT(hash);
+  }
+}
+
+int hp_monitored_functions_filter_collision(uint8_t hash, uint8_t *monitored_function_filter) {
+  uint8_t mask = INDEX_2_BIT(hash);
+  return monitored_function_filter[INDEX_2_BYTE(hash)] & mask;
 }
 
 int main(int argc, char const *argv[]) {
-  char *functions = get_functions();
-  char *function, *saveptr;
-  struct timeval start;
-  struct timeval end;
+  uint8_t monitored_function_filter[QUANTA_MON_MONITORED_FUNCTION_FILTER_SIZE];
+  char *monitored_functions[QUANTA_MON_MAX_MONITORED_FUNCTIONS];
+  struct method *methods = get_methods();
+  struct method *methods_save = methods;
+  struct timeval start, end;
   int count = 0;
 
+  bzero(monitored_function_filter, QUANTA_MON_MONITORED_FUNCTION_FILTER_SIZE);
+  hp_fill_monitored_functions(monitored_functions);
+  hp_monitored_functions_filter_init(monitored_functions, monitored_function_filter);
   gettimeofday(&start, NULL);
-  for (function = strtok_r(functions, "\n", &saveptr)
-  ; function; function = strtok_r(NULL, "\n", &saveptr)) {
-    ++count;
-    hp_match_monitored_function(function);
+  while (methods->function_name) {
+    // printf("Matching %s::%s\n", methods->function_name, methods->class_name);
+    if (hp_match_monitored_function(methods->function_name, methods->class_name) != -1)
+      ++count;
+    ++methods;
   }
   gettimeofday(&end, NULL);
-  printf("%d count\n", count);
-  printf("%ldmis\n", (end.tv_sec - start.tv_sec) * 1000000 + end.tv_usec - start.tv_usec);
+  printf("%d count, %fms\n", count, ((end.tv_sec -
+    start.tv_sec) * 1000000 + end.tv_usec - start.tv_usec) / 1000.0);
+  methods = methods_save;
+  count = 0;
+  gettimeofday(&start, NULL);
+  while (methods->function_name) {
+    if (hp_monitored_functions_filter_collision(
+    hp_inline_hash(methods->function_name), monitored_function_filter))
+      ++count;
+    ++methods;
+  }
+  gettimeofday(&end, NULL);
+  printf("%d count, %fms\n", count, ((end.tv_sec -
+    start.tv_sec) * 1000000 + end.tv_usec - start.tv_usec) / 1000.0);
   return 0;
 }
--- extension/src/computed_functions_filter.c
+++ extension/src/computed_functions_filter.c
@@ -25,7 +25,7 @@ void hp_fill_monitored_functions(char **function_names) {
  function_names[22] = NULL;
 }
 
-int hp_match_monitored_function(const char* function_name) {
+int hp_match_monitored_function(const char* function_name, const char *class_name) {
  if (function_name[0] == 'g') {
   if (function_name[1] == 'e') {
    if (function_name[2] == 't') {
@@ -39,7 +39,7 @@ int hp_match_monitored_function(const char* function_name) {
            if (function_name[10] == 'm') {
             if (function_name[11] == 'e') {
              if (function_name[12] == '\0') {
-              if (!strcmp(function_name, "Magento\\Framework\\App\\Request\\Http")) return 4;
+              if (!strcmp(class_name, "Magento\\Framework\\App\\Request\\Http")) return 4;
               return -1;
              }
              return -1;
@@ -81,7 +81,7 @@ int hp_match_monitored_function(const char* function_name) {
                    if (function_name[18] == 'k') {
                     if (function_name[19] == 's') {
                      if (function_name[20] == '\0') {
-                      if (!strcmp(function_name, "Magento\\Framework\\View\\Page\\Builder")) return 9;
+                      if (!strcmp(class_name, "Magento\\Framework\\View\\Page\\Builder")) return 9;
                       return -1;
                      }
                      return -1;
@@ -137,7 +137,7 @@ int hp_match_monitored_function(const char* function_name) {
            if (function_name[10] == 's') {
             if (function_name[11] == 'e') {
              if (function_name[12] == '\0') {
-              if (!strcmp(function_name, "Magento\\Framework\\App\\Response\\Http\\Interceptor")) return 12;
+              if (!strcmp(class_name, "Magento\\Framework\\App\\Response\\Http\\Interceptor")) return 12;
               return -1;
              }
              return -1;
@@ -177,7 +177,7 @@ int hp_match_monitored_function(const char* function_name) {
            if (function_name[10] == 'c') {
             if (function_name[11] == 'k') {
              if (function_name[12] == '\0') {
-              if (!strcmp(function_name, "Magento\\Framework\\View\\Layout")) return 15;
+              if (!strcmp(class_name, "Magento\\Framework\\View\\Layout")) return 15;
               return -1;
              }
              return -1;
@@ -215,7 +215,7 @@ int hp_match_monitored_function(const char* function_name) {
          if (function_name[8] == 'z') {
           if (function_name[9] == 'e') {
            if (function_name[10] == '\0') {
-            if (!strcmp(function_name, "Magento\\Framework\\Interception\\Config\\Config")) return 3;
+            if (!strcmp(class_name, "Magento\\Framework\\Interception\\Config\\Config")) return 3;
             return -1;
            }
            return -1;
@@ -245,7 +245,7 @@ int hp_match_monitored_function(const char* function_name) {
      if (function_name[4] == 't') {
       if (function_name[5] == 'e') {
        if (function_name[6] == '\0') {
-        if (!strcmp(function_name, "Magento\\Framework\\App\\Bootstrap")) return 1;
+        if (!strcmp(class_name, "Magento\\Framework\\App\\Bootstrap")) return 1;
         return -1;
        }
        if (function_name[6] == 'A') {
@@ -260,7 +260,7 @@ int hp_match_monitored_function(const char* function_name) {
                 if (function_name[15] == 'o') {
                  if (function_name[16] == 'n') {
                   if (function_name[17] == '\0') {
-                   if (!strcmp(function_name, "Magento\\Framework\\App\\Bootstrap")) return 2;
+                   if (!strcmp(class_name, "Magento\\Framework\\App\\Bootstrap")) return 2;
                    return -1;
                   }
                   return -1;
@@ -304,7 +304,7 @@ int hp_match_monitored_function(const char* function_name) {
         if (function_name[7] == 'p') {
          if (function_name[8] == 'e') {
           if (function_name[9] == '\0') {
-           if (!strcmp(function_name, "Magento\\Framework\\App\\Cache\\TypeList")) return 18;
+           if (!strcmp(class_name, "Magento\\Framework\\App\\Cache\\TypeList")) return 18;
            return -1;
           }
           return -1;
@@ -333,9 +333,9 @@ int hp_match_monitored_function(const char* function_name) {
       if (function_name[5] == 't') {
        if (function_name[6] == 'e') {
         if (function_name[7] == '\0') {
-         if (!strcmp(function_name, "PDOStatement")) return 16;
-         if (!strcmp(function_name, "Magento\\Backend\\Controller\\Adminhtml\\Cache\\FlushAll")) return 17;
-         if (!strcmp(function_name, "Magento\\Backend\\Controller\\Adminhtml\\Cache\\FlushSystem")) return 19;
+         if (!strcmp(class_name, "PDOStatement")) return 16;
+         if (!strcmp(class_name, "Magento\\Backend\\Controller\\Adminhtml\\Cache\\FlushAll")) return 17;
+         if (!strcmp(class_name, "Magento\\Backend\\Controller\\Adminhtml\\Cache\\FlushSystem")) return 19;
          return -1;
         }
         return -1;
@@ -361,8 +361,8 @@ int hp_match_monitored_function(const char* function_name) {
        if (function_name[6] == 'c') {
         if (function_name[7] == 'h') {
          if (function_name[8] == '\0') {
-          if (!strcmp(function_name, "Magento\\Framework\\App\\FrontController\\Interceptor")) return 7;
-          if (!strcmp(function_name, "Magento\\Framework\\App\\Action\\Action")) return 8;
+          if (!strcmp(class_name, "Magento\\Framework\\App\\FrontController\\Interceptor")) return 7;
+          if (!strcmp(class_name, "Magento\\Framework\\App\\Action\\Action")) return 8;
           return -1;
          }
          return -1;
@@ -394,7 +394,7 @@ int hp_match_monitored_function(const char* function_name) {
            if (function_name[10] == 'l') {
             if (function_name[11] == 't') {
              if (function_name[12] == '\0') {
-              if (!strcmp(function_name, "Magento\\Framework\\View\\Result\\Page\\Interceptor")) return 10;
+              if (!strcmp(class_name, "Magento\\Framework\\View\\Result\\Page\\Interceptor")) return 10;
               return -1;
              }
              return -1;
@@ -422,7 +422,7 @@ int hp_match_monitored_function(const char* function_name) {
   if (function_name[1] == 'u') {
    if (function_name[2] == 'n') {
     if (function_name[3] == '\0') {
-     if (!strcmp(function_name, "Magento\\Framework\\App\\Bootstrap")) return 0;
+     if (!strcmp(class_name, "Magento\\Framework\\App\\Bootstrap")) return 0;
      return -1;
     }
     return -1;
