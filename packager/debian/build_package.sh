01860b6e4b82927fdaaefe9674b729ec87c71c7a
Matthieu Rosinski <korrigan@slash16.org>
2016-09-06 18:05:31 +0200

    moved packaging stuff in packager dir

---
+++ packager/debian/build_package.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+package=$1
+basedir=/php-module
+packagerdir=/php-module/packager/debian
+
+if [ -z "$package" ]; then
+  echo "Usage: build_package.sh <package>"
+  exit 1
+fi
+
+echo "Creating build directory"
+rm -Rf ${packagedir}/${package}/build
+cp -r ${basedir}/extension ${packagerdir}/${package}/build
+cp -r ${packagerdir}/${package}/debian ${packagerdir}/${package}/build
+cd ${packagerdir}/${package}/build
+echo "Building package"
+debuild -us -uc
