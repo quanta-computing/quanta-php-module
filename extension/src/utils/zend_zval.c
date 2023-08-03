54a142c7eb832f3f9a561fef92cecfe78e924ae5
Matthieu Rosinski <korrigan@slash16.org>
2023-08-03 17:36:12 +0200

    fixed zend_read_property usage for PHP8

--- extension/src/utils/zend_zval.c
+++ extension/src/utils/zend_zval.c
@@ -3,7 +3,9 @@
 zval *zend_read_property_compat(zend_class_entry *ce, zval *obj, const char *name) {
 #if PHP_MAJOR_VERSION < 7
   return zend_read_property(ce, obj, name, strlen(name), 1);
-#else
+#elif PHP_MAJOR_VERSION < 8
   return zend_read_property(ce, obj, name, strlen(name), 1, NULL);
+#else
+  return zend_read_property(ce, Z_OBJ_P(obj), name, strlen(name), 1, NULL);
 #endif
 }
