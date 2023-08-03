eacb0f76b62592b073a065cc184831967cd894ae
Matthieu Rosinski <korrigan@slash16.org>
2023-08-03 17:36:49 +0200

    fixed PHP8 compat for hijacking zend_compile_string

--- extension/src/profiler/zend_execute.c
+++ extension/src/profiler/zend_execute.c
@@ -22,8 +22,14 @@ static void (*_zend_execute_internal) (zend_execute_data *execute_data, zval *re
 /* Pointer to the original compile function */
 static zend_op_array * (*_zend_compile_file) (zend_file_handle *file_handle, int type);
 
+#if PHP_VERSION_ID >= 80200
+static zend_op_array * (*_zend_compile_string) (zend_string *source_string, const char *filename, zend_compile_position position);
 /* Pointer to the original compile string function (used by eval) */
+#elif PHP_VERSION_ID >= 80000
+static zend_op_array * (*_zend_compile_string) (zend_string *source_string, const char *filename);
+#else
 static zend_op_array * (*_zend_compile_string) (zval *source_string, char *filename);
+#endif
 
 
 void hp_restore_original_zend_execute(void) {
@@ -196,11 +202,15 @@ ZEND_DLEXPORT zend_op_array* hp_compile_file(zend_file_handle *file_handle, int 
 
   const char     *filename;
   char           *func;
-  int             len;
+  size_t             len;
   zend_op_array  *ret;
   int             hp_profile_flag = 1;
 
+#if PHP_VERSION_ID < 80100
   filename = hp_get_base_filename(file_handle->filename);
+#else
+  filename = hp_get_base_filename(ZSTR_VAL(file_handle->filename));
+#endif
   len = strlen("load") + strlen(filename) + 3;
   func = (char *)emalloc(len);
   snprintf(func, len, "load::%s", filename);
@@ -233,7 +243,11 @@ ZEND_DLEXPORT zend_op_array* hp_compile_string(zval *source_string, char *filena
     snprintf(func, len, "eval::%s", filename);
 
     hp_profile_flag = hp_begin_profiling(&hp_globals.entries, func, NULL);
+#if PHP_VERSION_ID < 80200
     ret = _zend_compile_string(source_string, filename);
+#else
+    ret = _zend_compile_string(source_string, filename, position);
+#endif
     hp_end_profiling(&hp_globals.entries, hp_profile_flag, NULL);
     efree(func);
     return ret;
